9f79f19856616f12bbaf0c916774119a5b6739e9
Galanyksa1@gmail.com <Galanyksa1@gmail.com>
2021-10-05 09:53:14 +0300

    Cpy coDE

--- app.js
+++ app.js
@@ -1,13 +1,54 @@
+// const btnEl = document.querySelector('#btn');
+// const inputEl = document.querySelector('#input');
+// const containerEl = document.querySelector('#container');
+
+// btnEl.addEventListener("click", onClick);
+// containerEl.addEventListener("click", changeColor);
+
+// function onClick() {
+
+//     const data = inputEl.value;
+//     if (data) {
+//         createElement(data, containerEl, 'div', 'item-container');
+//         clearValue(inputEl);
+//     } else {
+//         clearValue(inputEl);
+//     }
+// }
+
+// function createElement(title, containerEl, tag, classList) {
+//     const element = document.createElement(tag);
+//     const buttonEl = document.createElement('button');
+//     element.textContent = title;
+//     buttonEl.textContent = 'X';
+//     buttonEl.addEventListener("click", deleteElement);
+
+//     element.classList.add(classList);
+//     element.appendChild(buttonEl)
+//     containerEl.append(element);
+// }
+
+// function deleteElement(e) {
+//     e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+// }
+
+// function changeColor(e) {
+//     e.target.classList.toggle('item-container-active')
+// }
+
+// function clearValue(element) {
+//     element.value = '';
+// }
+
 const btnEl = document.querySelector('#btn');
 const inputEl = document.querySelector('#input');
 const containerEl = document.querySelector('#container');
-
 btnEl.addEventListener("click", onClick);
 containerEl.addEventListener("click", changeColor);
 
 function onClick() {
-
-    const data = inputEl.value;
+    const data = inputEl.value.trim();
+    createElementButton(containerEl, 'button', 'button');
     if (data) {
         createElement(data, containerEl, 'div', 'item-container');
         clearValue(inputEl);
@@ -18,14 +59,21 @@ function onClick() {
 
 function createElement(title, containerEl, tag, classList) {
     const element = document.createElement(tag);
-    const buttonEl = document.createElement('button');
+    // const buttonEl = document.createElement('button');
     element.textContent = title;
+    // buttonEl.textContent = 'X';
+    // buttonEl.addEventListener("click", deleteElement);
+    element.classList.add(classList);
+    // element.appendChild(buttonEl)
+    containerEl.append(element);
+}
+
+function createElementButton(containerEl, tag, classList) {
+    const buttonEl = document.createElement(tag);
     buttonEl.textContent = 'X';
     buttonEl.addEventListener("click", deleteElement);
-
-    element.classList.add(classList);
-    element.appendChild(buttonEl)
-    containerEl.append(element);
+    buttonEl.classList.add(classList);
+    containerEl.append(buttonEl);
 }
 
 function deleteElement(e) {
@@ -39,51 +87,3 @@ function changeColor(e) {
 function clearValue(element) {
     element.value = '';
 }
-
-// const btnEl=document.querySelector('#btn');
-// const inputEl=document.querySelector('#input');
-// const containerEl=document.querySelector('#container');
-// btnEl.addEventListener("click", onClick);
-// containerEl.addEventListener("click", changeColor);
-// function onClick() {
-//     const data=inputEl.value.trim();
-//     createElementButton(containerEl, 'button', 'button');
-//     if (data) {
-//         createElement(data, containerEl, 'div', 'item-container');
-//         clearValue(inputEl);
-//     }
-//     else {
-//         clearValue(inputEl);
-//     }
-// }
-
-// function createElement(title, containerEl, tag, classList) {
-//     const element=document.createElement(tag);
-//     // const buttonEl = document.createElement('button');
-//     element.textContent=title;
-//     // buttonEl.textContent = 'X';
-//     // buttonEl.addEventListener("click", deleteElement);
-//     element.classList.add(classList);
-//     // element.appendChild(buttonEl)
-//     containerEl.append(element);
-// }
-
-// function createElementButton(containerEl, tag, classList) {
-//     const buttonEl=document.createElement(tag);
-//     buttonEl.textContent='X';
-//     buttonEl.addEventListener("click", deleteElement);
-//     buttonEl.classList.add(classList);
-//     containerEl.append(buttonEl);
-// }
-
-// function deleteElement(e) {
-//     e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-// }
-
-// function changeColor(e) {
-//     e.target.classList.toggle('item-container-active')
-// }
-
-// function clearValue(element) {
-//     element.value='';
-// }
