debfee7cb69a88710a4f1328d194cf1058bfc6cc
Galanyksa1@gmail.com <Galanyksa1@gmail.com>
2021-10-07 23:48:24 +0300

    removeElement

--- app.js
+++ app.js
@@ -1,45 +1,3 @@
-// const btnEl = document.querySelector('#btn');
-// const inputEl = document.querySelector('#input');
-// const containerEl = document.querySelector('#container');
-
-// btnEl.addEventListener("click", onClick);
-// containerEl.addEventListener("click", changeColor);
-
-// function onClick() {
-
-//     const data = inputEl.value;
-//     if (data) {
-//         createElement(data, containerEl, 'div', 'item-container');
-//         clearValue(inputEl);
-//     } else {
-//         clearValue(inputEl);
-//     }
-// }
-
-// function createElement(title, containerEl, tag, classList) {
-//     const element = document.createElement(tag);
-//     const buttonEl = document.createElement('button');
-//     element.textContent = title;
-//     buttonEl.textContent = 'X';
-//     buttonEl.addEventListener("click", deleteElement);
-
-//     element.classList.add(classList);
-//     element.appendChild(buttonEl)
-//     containerEl.append(element);
-// }
-
-// function deleteElement(e) {
-//     e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-// }
-
-// function changeColor(e) {
-//     e.target.classList.toggle('item-container-active')
-// }
-
-// function clearValue(element) {
-//     element.value = '';
-// }
-
 const btnEl = document.querySelector('#btn');
 const inputEl = document.querySelector('#input');
 const containerEl = document.querySelector('#container');
@@ -48,8 +6,7 @@ btnEl.addEventListener("click", onClick);
 containerEl.addEventListener("click", deleteElement);
 containerEl.addEventListener("click", changeColor);
 
-function onClick(e) {
-    // console.log(e.target.id)
+function onClick() {
     const data = inputEl.value.trim();
     createElementButton(containerEl, 'button', 'button');
     if (data) {
@@ -58,7 +15,6 @@ function onClick(e) {
     } else {
         clearValue(inputEl);
     }
-
 }
 
 function createElement(title, containerEl, tag, classList) {
@@ -80,7 +36,6 @@ function createElementButton(containerEl, tag, classList) {
 }
 
 function deleteElement(e) {
-    //e.target.parentNode.parentNode.removeChild(e.target.parentNode);
     const elements = containerEl.children;
     for (let i = 0; i < elements.length; i += 2) {
         if (e.target.id && elements[i].id && elements[i].id === e.target.id) {
@@ -90,30 +45,18 @@ function deleteElement(e) {
 }
 
 function removeElement(index, container) {
-    let shift = 1;
-    console.log("delete: " + index);
-    console.log(container.length);
-    container[index].parentNode.removeChild(container[index + shift])
-    console.log(container.length);
-    container[index].parentNode.removeChild(container[index])
-    console.log(container.length);
+    const shift = 1;
+    container[index].parentNode.removeChild(container[index + shift]);
+    container[index].parentNode.removeChild(container[index]);
 }
-
-// function changeColor(e) {
-//     e.target.classList.toggle('item-container-active');
-
-// }
 
 function clearValue(element) {
     element.value = '';
 }
 
-// 41.5
-
 function changeColor(e) {
     const elements = containerEl.children;
     for (i = 0; i < elements.length; i++) {
-        //  console.log(elements[i].id);
         if (e.target.id && elements[i].id && elements[i].id === e.target.id) {
             elements[i].classList.toggle('item-container-active');
         }
